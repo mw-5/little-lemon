f7669cee8354b22d02f43f47b5b769d21070ae03
mw <martinez2@gmx.de>
2025-07-10 20:09:23 +0200

    feat: Add logo to header

--- src/features/Header/Header.jsx
+++ src/features/Header/Header.jsx
@@ -1,5 +1,9 @@
+import logo from '../../assets/Logo.svg';
+
 export const Header = () => {
     return (
-        <header></header>
+        <header>
+            <img src={logo} alt="Little Lemon Logo" />
+        </header>
     )
 }
