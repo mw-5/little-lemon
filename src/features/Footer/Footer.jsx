f62e7cd45a062edeb02cf071e9a3fd716feabec7
mw <martinez2@gmx.de>
2025-07-15 11:37:11 +0200

    feat: Add contetn to footer

--- src/features/Footer/Footer.jsx
+++ src/features/Footer/Footer.jsx
@@ -1,5 +1,30 @@
+import logo from '../../assets/logo.svg';
+
 export const Footer = () => {
     return (
-        <footer></footer>
+        <footer>
+            <img src={logo} alt="Little Lemon Logo" />
+            <p>&copy; 2025 Little Lemon. All rights reserved.</p>
+            <section>
+                <Nav />
+            </section>
+            <section>
+                <h2>Contact</h2>
+                <p>
+                    address: 1st street, Chicago<br>
+                    phone number: 123</br>
+                    email: <a href="mailto:little@lemon.com">little@lemon.com</a>
+                </p>
+            </section>
+            <section>
+                <p>Follow us on social media!</p>
+                <nav>
+                    <ul>
+                        <li><a>Facebook</a></li>
+                        <li><a>Instagram</a></li>
+                    </ul>
+                </nav>
+            </section>
+        </footer>
     )
 }
