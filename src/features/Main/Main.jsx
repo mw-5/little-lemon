033f494ed8aec7a4df410f49db48ae2d36e1185b
mw <martinez2@gmx.de>
2025-07-17 11:30:15 +0200

    feat: Add grid layout to Main component

--- src/features/Main/Main.jsx
+++ src/features/Main/Main.jsx
@@ -1,6 +1,12 @@
+import './Main.css';
+
 export const Main = () => {
     return (
-        <main>
+        <main className="main">
+            <section style={{backgroundColor: "red"}}>hero</section>
+            <section style={{backgroundColor: "blue"}}>highlights</section>
+            <section style={{backgroundColor: "red"}}>testimonials</section>
+            <section style={{backgroundColor: "blue"}}>about</section>
         </main>
     );
 }
