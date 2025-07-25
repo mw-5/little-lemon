f9326099edde60394e9831d4bb650ee9d6978726
mw <martinez2@gmx.de>
2025-07-25 15:01:10 +0200

    feat: Add HomePage

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,8 @@
+import { HomePage } from '../../pages/HomePage/HomePage';
 import './Main.css';
 
 export const Main = () => {
     return (
-        <main className="main">
-            <section style={{backgroundColor: "red"}}>hero</section>
-            <section style={{backgroundColor: "blue"}}>highlights</section>
-            <section style={{backgroundColor: "red"}}>testimonials</section>
-            <section style={{backgroundColor: "blue"}}>about</section>
-        </main>
+        <HomePage />
     );
 }
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,5 @@
+export const HomePage = () => {
+    return (
+        <main className="home-page">Home</main>
+    );
+}
