a20d858596a52cd751610e6ec4f3b623ea749959
mw <martinez2@gmx.de>
2025-07-31 10:06:35 +0200

    refactor: Include Nav in Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,12 @@
+import './Header.css';
 import logo from '../../assets/Logo.svg';
+import { Nav } from '../Nav/Nav';
 
 export const Header = () => {
     return (
-        <header>
+        <header className='container'>
             <img src={logo} alt="Little Lemon Logo" />
+            <Nav className='nav-header' />
         </header>
     )
 }
