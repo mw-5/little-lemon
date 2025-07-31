883de1a7ed39ee6aaef5abbedd348332fe4fdd31
mw <martinez2@gmx.de>
2025-07-31 13:29:27 +0200

    feat: Set layout of headline of Specials

--- src/components/Specials/Specials.jsx
+++ src/components/Specials/Specials.jsx
@@ -1,8 +1,10 @@
+import './Specials.css';
+
 export const Specials = () => {
 	return (
-		<section>
-			<h4>This weeks specials!</h4>	
-			<button>Online Menu</button>
+		<section className="container specials">
+			<h1>This weeks specials!</h1>
+			<button>Online&nbsp;Menu</button>
 		</section>
 	)
 }
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,11 @@
 import { CallToAction } from "../../components/CallToAction/CallToAction";
+import { Specials } from "../../components/Specials/Specials";
 
 export const HomePage = () => {
     return (
         <main className="home-page">
             <CallToAction />
+            <Specials />
         </main>
     );
 }
