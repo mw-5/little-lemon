d3fd7c391e755d38768b17ba5cbdddbad07b1e7f
mw <martinez2@gmx.de>
2025-07-31 20:14:24 +0200

    feat: Put price on the right side of card

--- src/components/Specials/Card/Card.jsx
+++ src/components/Specials/Card/Card.jsx
@@ -1,9 +1,19 @@
+import './Card.css'
+
+const spread = {
+	display: 'flex',
+	flexDirection: 'row',
+	justifyContent: 'space-between'
+}
+
 export const Card = ({ id, image, title, price, description }) => {
 	return (
-		<article>
+		<article className="specials-card">
 			<img src={image} />
-			<h5>{title}</h5>
-			<span>{price}</span>
+			<div style={spread}>
+				<h5>{title}</h5>
+				<span>{price}</span>
+			</div>
 			<p>{description}</p>
 			Order online<img />
 		</article>
