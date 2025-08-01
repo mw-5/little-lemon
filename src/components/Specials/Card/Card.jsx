bd7f8f3bc8802554caa238445c5fe2e7527171e7
mw <martinez2@gmx.de>
2025-08-01 16:13:51 +0200

    feat: Style Specials Card

--- src/components/Specials/Card/Card.jsx
+++ src/components/Specials/Card/Card.jsx
@@ -1,21 +1,34 @@
 import './Card.css'
+import delivery from '../../../assets/Delivery.svg';
 
-const spread = {
+const flexbox = {
 	display: 'flex',
-	flexDirection: 'row',
+	flexDirection: 'row'
+}
+const spread = {
+	...flexbox,
 	justifyContent: 'space-between'
+}
+const flow = {
+	...flexbox,
+	justifyContent: 'flex-start',
 }
 
 export const Card = ({ id, image, title, price, description }) => {
 	return (
 		<article className="specials-card">
-			<img src={image} />
-			<div style={spread}>
-				<h5>{title}</h5>
-				<span>{price}</span>
+			<img src={image} alt='Image of a dish' />
+			<div>
+				<div style={spread}>
+					<h5>{title}</h5>
+					<span>{price}</span>
+				</div>
+				<p>{description}</p>
+				<div className='lead-text' style={flow}>
+					Order a delivery&nbsp;
+					<img src={delivery} alt='Image of a scooter' />
+				</div>
 			</div>
-			<p>{description}</p>
-			Order online<img />
 		</article>
 	)
 }
