9b85a444089ce3be4b9980f913b73170cc50b1e1
mw <martinez2@gmx.de>
2025-08-01 08:58:24 +0200

    feat: Set grid layout for Specials cards

--- src/components/Specials/Specials.jsx
+++ src/components/Specials/Specials.jsx
@@ -40,7 +40,7 @@ export const Specials = () => {
 		<section className="container specials">
 			<h1>This weeks specials!</h1>
 			<button>Online&nbsp;Menu</button>
-			<section>{cards}</section>
+			{cards}
 		</section>
 	)
 }
