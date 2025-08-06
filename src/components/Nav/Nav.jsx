b011f4960db11500772030e9b23f8750720009e1
mw <martinez2@gmx.de>
2025-08-06 17:00:11 +0200

    feat: Implement Home and Reservations links

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,13 +1,15 @@
+import { HomePage } from '../../pages/HomePage/HomePage';
 import './Nav.css';
+import { Link } from 'react-router-dom';
 
 export const Nav = ({ className }) => {
     return (
         <nav>
             <ul className={className}>
-                <li><a>Home</a></li>
+                <li><Link to="/">Home</Link></li>
                 <li><a>About</a></li>
                 <li><a>Menu</a></li>
-                <li><a>Reservations</a></li>
+                <li><Link to="/booking">Reservations</Link></li>
                 <li><a>Order Online</a></li>
                 <li><a>Login</a></li>
             </ul>
