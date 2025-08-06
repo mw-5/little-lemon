15d8c1813463065f1282b5a385b0d589986ac5bb
mw <martinez2@gmx.de>
2025-08-06 22:15:13 +0200

    refactor: Replace div with semantic tag

--- src/components/CallToAction/CallToAction.jsx
+++ src/components/CallToAction/CallToAction.jsx
@@ -4,12 +4,12 @@ import './CallToAction.css';
 export const CallToAction = () => {
     return (
         <section className='container call-to-action'>
-            <div>
+            <article>
                 <h1>Little&nbsp;Lemon</h1>
                 <h2>Chicago</h2>
                 <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
                 <button>Reserve a table</button>
-            </div>
+            </article>
             <img src={hero} alt="Image of bruchetta"></img>
         </section>
     );
