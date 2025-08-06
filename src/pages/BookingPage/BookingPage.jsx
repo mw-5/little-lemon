21ed738a6230e3039a98b373a89213b225843c07
mw <martinez2@gmx.de>
2025-08-06 16:06:28 +0200

    feat: Add BookingPage

---
+++ src/pages/BookingPage/BookingPage.jsx
@@ -0,0 +1,8 @@
+export const BookingPage = () => {
+    return (
+        <main>
+            <h1>Booking Page</h1>
+            <p>This is where users can book a table at Little Lemon.</p>
+        </main>
+    );
+}
