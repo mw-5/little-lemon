0446d8b6ad04bcdc50ddd4f19d2f32ca20b7395d
mw <martinez2@gmx.de>
2025-08-06 16:31:08 +0200

    feat: Setup Routes

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,12 @@
+import { Routes, Route } from 'react-router-dom';
 import { HomePage } from '../../pages/HomePage/HomePage';
+import { BookingPage } from '../../pages/BookingPage/BookingPage';
 
 export const Main = () => {
     return (
-        <HomePage />
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/booking" element={<BookingPage />} />
+        </Routes>
     );
 }
