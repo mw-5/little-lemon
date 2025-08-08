d9d172ba8ca1cb8cef4253f5074b5fcfa4617566
mw <martinez2@gmx.de>
2025-08-08 16:17:02 +0200

    test: Update test on form submission

--- src/components/BookingForm/BookingForm.test.jsx
+++ src/components/BookingForm/BookingForm.test.jsx
@@ -15,15 +15,14 @@ test('Renders the booking form', () => {
 test('User can submit form', () => {
     // Arrange
     const mockDispatch = jest.fn();
-    render(<BookingForm availableTimes={['17:00', '18:00']} dispatch={mockDispatch} />);
+    const mockSubmitForm = jest.fn();
+    render(<BookingForm availableTimes={['17:00', '18:00']} dispatch={mockDispatch} submitForm={mockSubmitForm} />);
     const form = screen.getByRole('form', { name: /table reservation form/i });
     const dateInput = screen.getByLabelText('Choose date');
     const timeSelect = screen.getByLabelText('Choose time');
     const guestsInput = screen.getByLabelText('Number of guests');
     const occasionSelect = screen.getByLabelText('Occasion');
     const submitButton = screen.getByText('Make Your reservation');
-    const submitEvent = new Event('submit', { bubbles: true, cancelable: true });
-    const preventDefaultSpy = jest.spyOn(submitEvent, 'preventDefault');
 
     // Act
     fireEvent.change(dateInput, { target: { value: '2023-10-01' } });
@@ -31,8 +30,7 @@ test('User can submit form', () => {
     fireEvent.change(guestsInput, { target: { value: '2' } });
     fireEvent.change(occasionSelect, { target: { value: 'Anniversary' } });
     fireEvent.click(submitButton);
-    form.dispatchEvent(submitEvent);
 
     // Assert
-    expect(preventDefaultSpy).toHaveBeenCalled();
+    expect(mockSubmitForm).toHaveBeenCalled();
 });
