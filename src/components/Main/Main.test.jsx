9f9cf3c6951d46dc09b93059e762e3625a407340
mw <martinez2@gmx.de>
2025-08-08 16:20:59 +0200

    refactor: Organize initializeTimes tests into describe block

--- src/components/Main/Main.test.jsx
+++ src/components/Main/Main.test.jsx
@@ -1,15 +1,25 @@
 import { initializeTimes, updateTimes } from './Main';
 import { fetchAPI } from '../../utils/api';
 
-test('initializeTimes returns correct times', () => {
-    // Arrange
-    const expectedTimes = fetchAPI(new Date());
+describe('initializeTimes', () => {
+    it('should return correct times', () => {
+        // Arrange
+        const expectedTimes = fetchAPI(new Date());
 
-    // Act
-    const times = initializeTimes();
+        // Act
+        const times = initializeTimes();
 
-    // Assert
-    expect(times).toEqual(expectedTimes);
+        // Assert
+        expect(times).toEqual(expectedTimes);
+    });
+
+    it('should return a non-empty array', () => {
+        // Act
+        const times = initializeTimes();
+
+        // Assert
+        expect(times.length).toBeGreaterThan(0);
+    });
 });
 
 test('updateTimes returns changed state', () => {
