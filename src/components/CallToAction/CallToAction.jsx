2f2fdb0eaf2f8ec634faa1510361bfd36f3f2699
mw <martinez2@gmx.de>
2025-08-14 22:44:45 +0200

    feat: Add navigation to booking page in CallToAction component

--- src/components/CallToAction/CallToAction.jsx
+++ src/components/CallToAction/CallToAction.jsx
@@ -1,14 +1,17 @@
+import { useNavigate } from 'react-router-dom';
 import hero from '../../assets/restauranfood.jpg';
 import './CallToAction.css';
 
 export const CallToAction = () => {
+    const navigate = useNavigate();
+
     return (
         <section className='container call-to-action'>
             <article>
                 <h1>Little&nbsp;Lemon</h1>
                 <h2>Chicago</h2>
                 <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
-                <button>Reserve a table</button>
+                <button onClick={() => navigate('/booking')}>Reserve a table</button>
             </article>
             <img src={hero} alt="Image of bruchetta"></img>
         </section>
