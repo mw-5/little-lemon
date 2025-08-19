bbcfff7a82bb0b81928d8f8f511f8def1070895c
mw <martinez2@gmx.de>
2025-08-19 12:39:57 +0200

    feat: Align Footer at bottom

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import { Header } from './components/Header/Header';
 
 function App() {
   return (
-    <>
+    <div className="app">
       <Header />
       <Main />
       <Footer />
-    </>
+    </div>
   );
 }
 
