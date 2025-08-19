7d8bced3156283bbb2f6f148c1c70840d3f61f1e
mw <martinez2@gmx.de>
2025-08-19 11:29:05 +0200

    fix: Correct hidden hamburger menu being displayed right

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -22,7 +22,7 @@ export const Nav = ({ className }) => {
                 <li><Link to="/#about">About</Link></li>
                 <li><Link to="/menu">Menu</Link></li>
                 <li><Link to="/booking">Reservations</Link></li>
-                <li><Link to="/order-online">Order Online</Link></li>
+                <li><Link to="/order-online">Order&nbsp;Online</Link></li>
                 <li><Link to="/login">Login</Link></li>
             </ul>
         </nav>
